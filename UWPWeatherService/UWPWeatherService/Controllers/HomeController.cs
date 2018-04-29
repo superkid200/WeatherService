66fb034e2bcd526307d79d33a48904a39143ce13
superkid200 <minh306@outlook.com.vn>
2018-04-30 00:54:38 +0700

    Updated Code

---
+++ UWPWeatherService/UWPWeatherService/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using UWPWeatherService.Models;
+
+namespace UWPWeatherService.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async Task<ActionResult> Index(string lat, string lon)
+        {
+            double latitude = double.Parse(lat, CultureInfo.InvariantCulture);
+            double longitude = double.Parse(lon, CultureInfo.InvariantCulture);
+
+            var weather = await OpenWeatherMapProxy.GetWeather(latitude, longitude);
+            ViewBag.Temp = weather.main.temp.ToString();
+            ViewBag.Description = weather.weather[0].description;
+            ViewBag.Name = weather.name;
+            return View();
+        }
+    }
+}
